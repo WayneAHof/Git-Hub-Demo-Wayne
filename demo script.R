9e6b5cd9733fc65d81ba28c3ec5deecd30596c04
WayneAHof <Wayne.Hofmann@gmail.com>
2021-05-21 10:07:25 +1200

    Demo script added notes

--- demo script.R
+++ demo script.R
@@ -1,1 +1,14 @@
 # this is a demo of Github workflow
+
+# Lets assume one folder on my computer =
+# 1 RStudio project =
+# 1 GitHub Repo
+# Then everything is sweet as!!
+
+# to get any project into a github repo, 
+# usethis::use_git() # connects project to git on your computer
+# usethis:: use_github() # connects git to github
+## Important options to consider for use_github()
+# 1 organization = "DairyNZ"
+# 2 private = TRUE # default is FALSE, i.e it will be public!!!
+
